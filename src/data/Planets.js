aa987504a4ed59bfcfb19b2973097e490d733617
camisbersann <camila.bersan@aluno.senai.br>
2024-03-18 17:16:11 -0300

    feat: start planets data

---
+++ src/data/Planets.js
@@ -0,0 +1,19 @@
+const Planets = [
+    {
+        id: "1",
+        name: "Marte",
+        dataDaConquista: "08/12/2040",
+        corPrimaria: "#8b0000",
+        corSecundaria: "#964b00",
+        população: 2300000,
+        recursosNaturais: [
+           "Enxofre",
+           "Nitrogênio",
+           "Hidrogênio",
+           "Oxigênio",
+           "Fósforo",
+           "Carbono",
+        ]
+        
+    }
+]
