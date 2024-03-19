b839ff9171478af6aab697304f97c92035535ae9
camisbersann <camila.bersan@aluno.senai.br>
2024-03-19 15:53:36 -0300

    feat: finish planet data

--- src/data/Planets.js
+++ src/data/Planets.js
@@ -5,7 +5,7 @@ const Planets = [
         dataDaConquista: "08/12/2040",
         corPrimaria: "#8b0000",
         corSecundaria: "#964b00",
-        população: 2300000,
+        população: 0,
         recursosNaturais: [
            "Enxofre",
            "Nitrogênio",
@@ -13,7 +13,17 @@ const Planets = [
            "Oxigênio",
            "Fósforo",
            "Carbono",
+        ],
+        numeroAssentamentos: 2300000,
+        localização: [
+            {galáxia: "Via Láctea",
+             sistemaSolar: "Quarto planeta a partir do Sol",
+             coodernadas: "13° 20´ 4",
+        }
+        ],
+        governante: [
+            {nome: "Arial", 
+             título: "Imperatriz Suprema da Cidade Vermelha"}
         ]
-        
     }
 ]
