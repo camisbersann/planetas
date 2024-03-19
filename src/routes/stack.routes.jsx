c54684a3367534832b70889b156cca3ba4bedfbd
camisbersann <camila.bersan@aluno.senai.br>
2024-03-19 17:00:35 -0300

    feat: fix errors

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PlanetsRegistered from "../screens/PlanetsRegistered";
+import Details from "../screens/Details";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,7 +8,7 @@ const StackRoutes = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="PlanetsRegistered" component={PlanetsRegistered} />
-      <Stack.Screen name="Details" component={PlanetsRegistered} />
+      <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   );
 };
