2d9310723ffaa34fdded640e6abd48d8ab7620ae
camisbersann <camila.bersan@aluno.senai.br>
2024-03-23 13:28:38 -0300

    feat: edit and delete

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,16 +1,26 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import PlanetClass from '../../models/planet';
 import { useNavigation } from "@react-navigation/native";
 import Title from '../../components/Title';
+import planetList from '../../models/planetList';
+import list from '../../models/planetList';
 
 export default function Details({ route }) {
   const navigation = useNavigation();
   const { data } = route.params;
-
   // console.log("Hereee", data);
 
-  
+  const editPlanet = () =>{
+    navigation.navigate("Home", { planet: data, edit: true});
+  };
+
+  const deletePlanet = () => {
+    list.remove(data.id);
+    navigation.navigate("PlanetsRegistered")
+  }
+
+
   return (
     <View style={styles.container}>
       <Title title="Detalhes"/>
@@ -26,6 +36,16 @@ export default function Details({ route }) {
     <Text>{data.dataConquista}</Text>
     <Text>{data.corPrimaria}</Text>
     <Text>{data.corSecundaria}</Text>
+    </View>
+
+    <View>
+      <TouchableOpacity style={styles.editButton} onPress={editPlanet}>
+        <Text>Editar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.deleteButton} onPress={deletePlanet}>
+        <Text>Remover</Text>
+      </TouchableOpacity>
     </View>
       
     </View>
