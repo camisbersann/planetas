cb2b1cc3fe52ae9d37e9efaefeed6b01bd241140
camisbersann <camila.bersan@aluno.senai.br>
2024-03-23 12:56:05 -0300

    feat: fix errors in planetList

--- src/models/planetList.js
+++ src/models/planetList.js
@@ -1,5 +1,5 @@
-//import PlanetClass from "./planet";
 import { Planet } from "../data/Planet";
+import PlanetClass from "./planet";
 
 class PlanetList {
   constructor() {
@@ -22,12 +22,22 @@ class PlanetList {
     return this.planets;
   }
 
-  update(id, name, dataConquista) {
-    const user = this.getPlanetById(id);
+  update(id, nomePlaneta, dataConquista, corPrimaria, corSecundaria, populacao, recursosNaturais, numeroAssentamentos, galaxia, sistemaSolar, coordenadas, nomeGovernante, titulo) {
+    const planet = this.getPlanetById(id);
 
-    if (user) {
-      user.name = name;
-      user.dataConquista = dataConquista;
+    if (planet) {
+      planet.nomePlaneta = nomePlaneta;
+      planet.dataConquista = dataConquista;
+      planet.corPrimaria = corPrimaria;
+      planet.corSecundaria = corSecundaria;
+      planet.populacao = populacao;
+      planet.recursosNaturais = recursosNaturais;
+      planet.numeroAssentamentos = numeroAssentamentos;
+      planet.galaxia = galaxia;
+      planet.sistemaSolar = sistemaSolar;
+      planet.coordenadas = coordenadas;
+      planet.nomeGovernante = nomeGovernante;
+      planet.titulo = titulo;
     }
 
     return user;
@@ -35,7 +45,8 @@ class PlanetList {
 }
 
 const list = new PlanetList();
+const newPlanet = new PlanetClass(Planet.nomePlaneta, Planet.corPrimaria, Planet.corSecundaria, Planet.população, Planet.recursosNaturais, Planet.numeroAssentamentos, Planet.galáxia, Planet.sistemaSolar, Planet.coodernadas, Planet.nomeGovenador, Planet.título)
 
-list.addPlanet(Planet);
+list.addPlanet(newPlanet);
 
-export default list
+export default list;
