5b6ec4d8d6e5ccc993f051902e940f5e0bc32cdd
camisbersann <camila.bersan@aluno.senai.br>
2024-03-25 14:44:31 -0300

    feat: change background image

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -100,7 +100,7 @@ export default function Home({ route }) {
   }
 
   return (
-    <ImageBackground source={require('../../../assets/galaxia.jpg')} style={styles.container}>
+    <ImageBackground source={require('../../../assets/galaxia01.jpeg')} style={styles.container}>
 
     <View style={styles.container}>
       <Title title={isUpdate ? "Editar Planeta" : "Cadastrar Planeta"} />
